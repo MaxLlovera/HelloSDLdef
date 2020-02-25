5be83730f5936bee62be6e1d4ac630db22d906cf
MaxLlovera <61042419+MaxLlovera@users.noreply.github.com>
2020-02-25 20:51:20 +0100

    quadrat moventse
    quadrat moventse

--- maincpp.cpp
+++ maincpp.cpp
@@ -1,33 +1,147 @@
 #include "SDL/include/SDL.h"
 #pragma comment(lib,"SDL/libx86/SDL2.lib")
 #pragma comment(lib,"SDL/libx86/SDL2main.lib")
-
+#include <stdio.h>
 #include <iostream>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+void logSDLError(std::ostream& os, const std::string& msg) {
+	os << msg << " error: " << SDL_GetError() << std::endl;
+}
+
+void clear(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+}
+
+void draw(SDL_Renderer* renderer, SDL_Rect* rect) {
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderFillRect(renderer, rect);
+	SDL_RenderPresent(renderer);
+}
 
 int main(int argc, char** argv) {
 
+	/*
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 
-
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+	// Initialize SDL.
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		return 1;
-	}
 
-	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-	if (win == nullptr) {
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+	// Create the window where we will draw.
+	window = SDL_CreateWindow("SDL_RenderClear",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		512, 512,
+		0);
 
-	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (ren == nullptr) {
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+	// We must call SDL_CreateRenderer in order for draw calls to affect this window.
+	renderer = SDL_CreateRenderer(window, -1, 0);
 
+	// Select the color for drawing. It is set to red here.
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+	// Clear the entire screen to our selected color.
+	SDL_RenderClear(renderer);
+
+	
+
+	// Up until now everything was drawn behind the scenes.
+	// This will show the new, red contents of the window.
+	SDL_RenderPresent(renderer);
+
+	// Give us time to see the window.
+	SDL_Delay(5000);
+
+	// Always be sure to clean up
+	SDL_Quit();
+
+
+	system("pause");
 	return 0;
+	*/
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	window = SDL_CreateWindow(
+		"An SDL2 window",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		640,
+		480,
+		SDL_WINDOW_OPENGL
+	);
+
+	if (window == NULL) {
+		printf("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (renderer == NULL) {
+		printf("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	SDL_Rect rectangle;
+	rectangle.x = 50;
+	rectangle.y = 50;
+	rectangle.w = 50;
+	rectangle.h = 50;
+
+	clear(renderer);
+
+	int quit = 0;
+	while (!quit) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_KEYDOWN) {
+				// Handle arrow keys
+				switch (event.key.keysym.scancode) {
+				case SDL_SCANCODE_LEFT:
+					rectangle.x -= 10;
+					break;
+				case SDL_SCANCODE_RIGHT:
+					rectangle.x += 10;
+					break;
+				case SDL_SCANCODE_UP:
+					rectangle.y -= 10;
+					break;
+				case SDL_SCANCODE_DOWN:
+					rectangle.y += 10;
+					break;
+				default:
+					break;
+				}
+
+			}
+			else if (event.type == SDL_QUIT) {
+				quit = 1;
+				puts("QUIT!");
+				break;
+			}
+		}
+
+		clear(renderer);
+		draw(renderer, &rectangle);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+
+
+
+	
 
 }
